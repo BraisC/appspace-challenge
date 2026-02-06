acf1acc0a58b589eda69cad670c1d0b6228499da
Brais Cao <braiscao.dev@gmail.com>
2026-02-06 18:01:52 +0100

    Cleanup and showing raw data

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+// import { useState } from 'react';
 import { useGetCharacters } from './hooks/useCharacters';
 
 function App() {
-  const [count, setCount] = useState(0);
   const { data, isLoading } = useGetCharacters({ page: 1 });
 
-  console.log({ data, isLoading });
+  if (isLoading) return <div>Loading...</div>;
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {data?.characters?.results?.map((character) => (
+          <div key={character?.id}>
+            <h2>{character?.name}</h2>
+            <p>{character?.status}</p>
+            <p>{character?.species}</p>
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
 }
