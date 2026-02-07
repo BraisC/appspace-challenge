8a1e762eabe5e8f06c902dcf96df6c967f0f4237
Brais Cao <braiscao.dev@gmail.com>
2026-02-07 16:56:06 +0100

    Add 'react-router` to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { CharacterList } from './pages/CharacterList';
 
 function App() {
-  return <CharacterList />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<CharacterList />} />
+        <Route path="/character/:id" element={<div />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
