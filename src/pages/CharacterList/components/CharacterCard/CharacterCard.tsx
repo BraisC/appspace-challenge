e878cd40fab291a4d71f92b2b2e88dbcc8489bb9
Brais Cao <braiscao.dev@gmail.com>
2026-02-08 17:02:33 +0100

    Added prefetch of `GetCharacter` on mouse hover in `CharacterCard`

--- src/pages/CharacterList/components/CharacterCard/CharacterCard.tsx
+++ src/pages/CharacterList/components/CharacterCard/CharacterCard.tsx
@@ -8,12 +8,24 @@ import {
   Name,
   Species,
 } from './CharacterCard.styles';
+import { useQueryClient } from '@tanstack/react-query';
+import { graphqlClient } from '@/lib/graphql-client';
+import { GetCharacterDocument } from '@/graphql/GetCharacters';
 
 type CharacterCardProps = Pick<Character, 'id' | 'name' | 'image' | 'species'>;
 
 export const CharacterCard = ({ character }: { character: CharacterCardProps }) => {
+  const queryClient = useQueryClient();
+
+  const handleMouseEnter = () => {
+    queryClient.prefetchQuery({
+      queryKey: ['GetCharacter', { id: character.id }],
+      queryFn: () => graphqlClient.request(GetCharacterDocument, { id: character.id }),
+    });
+  };
+
   return (
-    <CardLink to={`/character/${character.id}`}>
+    <CardLink to={`/character/${character.id}`} onMouseEnter={handleMouseEnter}>
       <Card>
         <ImageWrapper>
           <Image src={character.image ?? '/character-placeholder.jpg'} alt={character.name ?? ''} />
